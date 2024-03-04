e79eff29c0a5c48d6df75f5bd65beeb23e4478de
EbinaKai <blackbicycle55@gmail.com>
2024-03-04 13:41:55 +0900

    [update] hide db config

--- api/db_config.js
+++ api/db_config.js
@@ -1,9 +1,11 @@
+require("dotenv").config()
+
 exports.mysql_setting = {
-    connectionLimit: 33,
-    host: 'localhost',
-    user: 'dev',
-    password: 'sql',
-    database: 'dal_in_eng',
+    connectionLimit: 3,
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
 };
 
 exports.getAllArticleSQL = "SELECT * FROM article";
