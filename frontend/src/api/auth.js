d0335ea9b912e3ba9083796ec2b66f1277744f3e
EbinaKai <blackbicycle55@gmail.com>
2024-09-22 12:18:19 +0000

    [remove] Deprecated vulnerable packages and updated dependencies to enhance security.

--- frontend/src/api/auth.js
+++ frontend/src/api/auth.js
@@ -1,46 +1,39 @@
 import axios from "axios"
-import { baseUrl } from "../config"
-
-const config = {
-  method: null,
-  url: null,
-  headers: {},
-  data: null
-}
+import { baseUrl, request_config } from "../config"
 
 export default {
   login: authInfo => {
-    config.method = "post";
-    config.data = authInfo;
-    config.url = baseUrl + "/login";
+    request_config.method = "post";
+    request_config.data = authInfo;
+    request_config.url = baseUrl + "/login";
 
-    return axios.request(config)
+    return axios.request(request_config)
       .then( res => res )
       .catch( err => { throw err } );
   },
   logout: () => {
-    config.method = "delete";
-    config.url = baseUrl + "/logout";
+    request_config.method = "delete";
+    request_config.url = baseUrl + "/logout";
 
-    return axios.request(config)
+    return axios.request(request_config)
       .then( res => res )
       .catch( err => { throw err } );
   },
   createUser: userInfo => {
-    config.method = "post";
-    config.data = userInfo;
-    config.url = baseUrl + "/createuser";
+    request_config.method = "post";
+    request_config.data = userInfo;
+    request_config.url = baseUrl + "/createuser";
 
-    return axios.request(config)
+    return axios.request(request_config)
       .then( res => res )
       .catch( err => { throw err } );
   },
   checkToken: token => {
-    config.method = "post";
-    config.data = token;
-    config.url = baseUrl + "/verify";
+    request_config.method = "post";
+    request_config.data = token;
+    request_config.url = baseUrl + "/verify";
 
-    return axios.request(config)
+    return axios.request(request_config)
       .then( res => res )
       .catch( err => { throw err } );
   }
